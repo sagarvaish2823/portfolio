8803a825f048663c0a52b8e2e78561e84ac5038d
Sagar Vaish <68708676+sagarvaish2823@users.noreply.github.com>
2023-06-14 00:59:08 +0530

    clean file, tailwind screens, drakmode added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        standalone: { raw: "(display-mode: standalone)" },
+        "sm-280": "280px",
+        "sm-320": "320px",
+        "sm-360": "360px",
+        "sm-390": "390px",
+        "sm-414": "414px",
+        "sm-480": "480px",
+        "sm-540": "540px",
+        "sm-640": "640px",
+        "md-768": "768px",
+        "md-912": "912px",
+        "lg-1024": "1024px",
+        "lg-1200": "1200px",
+        "lg-1366": "1366px",
+        "xl-1440": "1440px",
+        "xl-1920": "1920px",
+        "xl-2560": "2560px",
       },
     },
   },
   plugins: [],
-}
+};
