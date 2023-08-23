1bdbe4a026816aa2f68478a1d7fadf76ad8492e4
Sagar Vaish <68708676+sagarvaish2823@users.noreply.github.com>
2023-08-24 00:32:04 +0530

    ew

--- src/components/Pages/Home/FifthSection/index.jsx
+++ src/components/Pages/Home/FifthSection/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ProjectWork = () => {
-  return <div className="h-screen">ProjectWork</div>;
+  return (
+    <section className="mx-8 h-screen pt-8 lg-1024:mt-16">
+      <div>
+        <h2 className="text-4xl font-bold tracking-tight lg-1024:pl-16 lg-1024:text-5xl">
+          My Work
+        </h2>
+      </div>
+    </section>
+  );
 };
 
 export default ProjectWork;
