58d9758ec4cbba44d367bb82b5339200460049e0
Sagar Vaish <68708676+sagarvaish2823@users.noreply.github.com>
2023-09-15 02:09:51 +0530

    hero links mobile optimsed

--- src/components/Pages/Home/FirstSection/index.jsx
+++ src/components/Pages/Home/FirstSection/index.jsx
@@ -17,17 +17,21 @@ const MainSection = () => {
         <h2 className="flex items-end justify-center text-4xl text-black drop-shadow-md dark:text-white md-768:text-6xl lg-1024:text-5xl">
           webistesbysagar
         </h2>
-        <div className="flex items-start justify-center gap-8">
-          <Link href={"/"} className="button_main hover:shadow-md">
-            Let&apos;s Make Your Website
-          </Link>
-          <Link href={"/audit-page"} className="button_main hover:shadow-md">
-            Your Free Website Audit
-          </Link>
+        <div className="mx-8 flex h-fit flex-wrap items-start justify-center gap-8">
+          <div>
+            <Link href={"/"} className="button_main hover:shadow-md">
+              Let&apos;s Make Your Website
+            </Link>
+          </div>
+          <div>
+            <Link href={"/audit-page"} className="button_main hover:shadow-md">
+              Your Free Website Audit
+            </Link>
+          </div>
         </div>
       </section>
       <m.section className="flex flex-col items-center justify-center">
-        <ToolTip text={"Mujhe Pata tha Piddi tum ispe hover kroge"}>
+        <ToolTip text={"Scroll Down"}>
           <BsMouse className="hidden text-4xl lg-1024:block" />
           <MdSwipeUp className="text-4xl lg-1024:hidden" />
         </ToolTip>
