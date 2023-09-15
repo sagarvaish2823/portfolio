6309610920e454c97b3e4d36e5b175a239455ceb
Sagar Vaish <68708676+sagarvaish2823@users.noreply.github.com>
2023-09-15 12:45:10 +0530

    preloader added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import HomePage from "@/components/Pages/Home";
+import Preloader from "@/components/Pages/PreLoader";
 
 export default function Home() {
-  return (
-    <section className="pb-8">
-      <HomePage />
-    </section>
-  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [setLoading]);
+
+  if (loading) {
+    return <Preloader />;
+  } else
+    return (
+      <section className="pb-8">
+        <HomePage />
+      </section>
+    );
 }
