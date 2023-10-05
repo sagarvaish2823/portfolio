7bd59089472f2ed3df46e742083d63dec91e4f0f
Sagar Vaish <68708676+sagarvaish2823@users.noreply.github.com>
2023-10-05 16:52:46 +0530

    darkMode

--- src/context/themeContext/themeContext.jsx
+++ src/context/themeContext/themeContext.jsx
@@ -1,53 +1,53 @@
 "use client";
-// import { createContext, useState } from "react";
-
-// export const ThemeContext = createContext();
-
-// export const ThemeProvider = ({ children }) => {
-//   const [mode, setMode] = useState("dark");
-
-//   const toggle = () => {
-//     setMode((prev) => (prev === "dark" ? "light" : "dark"));
-//   };
-//   return (
-//     <ThemeContext.Provider value={{ toggle, mode }}>
-//       <div className={`theme ${mode}`}>{children}</div>
-//     </ThemeContext.Provider>
-//   );
-// };
-
-import React, { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [mode, setMode] = useState(() => {
-    const initialTheme = localStorage.getItem("theme");
-    return initialTheme ? initialTheme : "dark";
-  });
-
-  function getThemeFromLocalStorage() {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      setMode(savedTheme);
-    }
-  }
+  const [mode, setMode] = useState("dark");
 
   const toggle = () => {
-    setMode((prevTheme) => {
-      const newTheme = prevTheme === "light" ? "dark" : "light";
-      localStorage.setItem("theme", newTheme);
-      return newTheme;
-    });
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
   };
-
-  useEffect(() => {
-    getThemeFromLocalStorage();
-  }, [mode]);
-
   return (
     <ThemeContext.Provider value={{ toggle, mode }}>
       <div className={`theme ${mode}`}>{children}</div>
     </ThemeContext.Provider>
   );
 };
+
+// import React, { createContext, useEffect, useState } from "react";
+
+// export const ThemeContext = createContext();
+
+// export const ThemeProvider = ({ children }) => {
+//   const [mode, setMode] = useState(() => {
+//     const initialTheme = localStorage.getItem("theme");
+//     return initialTheme ? initialTheme : "dark";
+//   });
+
+//   function getThemeFromLocalStorage() {
+//     const savedTheme = localStorage.getItem("theme");
+//     if (savedTheme) {
+//       setMode(savedTheme);
+//     }
+//   }
+
+//   const toggle = () => {
+//     setMode((prevTheme) => {
+//       const newTheme = prevTheme === "light" ? "dark" : "light";
+//       localStorage.setItem("theme", newTheme);
+//       return newTheme;
+//     });
+//   };
+
+//   useEffect(() => {
+//     getThemeFromLocalStorage();
+//   }, [mode]);
+
+//   return (
+//     <ThemeContext.Provider value={{ toggle, mode }}>
+//       <div className={`theme ${mode}`}>{children}</div>
+//     </ThemeContext.Provider>
+//   );
+// };
